c87e133f53898cf1763c81da22a03c5cfe05caba
Sokomine <wegwerf@anarres.dyndns.org>
2013-11-27 00:04:42 +0100

    added the windmills

---
+++ init.lua
@@ -0,0 +1,11 @@
+
+
+-- two very large windmills
+windmill.register_windmill( "windmill_large:large_windmill_sails", "Windmill sails (large)",
+			"windmill_wooden_cw_with_sails_304px.png", "windmill_wooden_ccw_with_sails_304px.png",
+			20.0, "windmill_wooden_inv.png", 1.0, "windmill:windmill_sails", 3 );
+
+windmill.register_windmill( "windmill_large:large_windmill_idle",  "Windmill idle (large)",
+			"windmill_wooden_cw_304px.png", "windmill_wooden_ccw_304px.png",
+			20.0, "windmill_wooden_no_sails_inv.png", 2.0, "windmill:windmill_idle", 3 );
+
